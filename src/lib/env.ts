2fdb9d5db9ea63f1ea2483a3bdefb15e0fcb29e0
LeonAlvarezz <hongnarethputponleu015@gmail.com>
2026-04-19 00:12:13 +0700

    Update env schema database url

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -12,7 +12,7 @@ const envSchema = z.object({
       z.literal("production"),
     ])
     .default("development"),
-  DATABASE_URL: z.string().startsWith("postgres://"),
+  DATABASE_URL: z.string().startsWith("postgres"),
   HASH_PASSWORD_ALGORITHM: z.enum(["argon2id", "argon2d", "argon2i", "bcrypt"]),
   HASH_PASSWORD_COST: z.coerce.number(),
   REDIS_URL: z.string(),
